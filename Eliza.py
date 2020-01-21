e3b20e472ea9d1d4e83d5accc64af4f45e46eb7a
Pete George <georgepj@vcu.edu>
2020-01-21 17:16:29 -0500

    First Name found, working on responding to first question

--- Eliza.py
+++ Eliza.py
@@ -3,20 +3,42 @@
 Created on Tue Jan 21 15:34:26 2020
 
 @author: Peter
+TODO:
+    Catch Gibberish
+    Catch Overly Discriptive
+    Catch Cursing
+    Catch emotions. {Sad, Angry, Happy}
+    
 """
 import re
 import random
 
 '''Catch their first name'''
-def catchName():
-    phrase = input("Hi, I'm a psychotherapist. What is your name?\n")
-    tokens = tokenize(phrase)
+def catchName(myNameIsPhrase):
+    tokens = tokenize(myNameIsPhrase)
+    for _ in tokens:
+        _ = _.lower()
+        if re.search(r'my|name|is|i\'m|i|am|they|call|me|you|can|nickname|go|by|sometimes|hello', _) is None:
+            return _
     return 
 
 '''Turn any string into an array of tokens based on white space(+)'''
 def tokenize(phrase):
     return re.split('\s+',phrase)
 
-client = catchName()
-print(client)
+'''--------------Init--------------'''
+phrase = input("Hi, I'm a psychotherapist. What is your name?\n")
+client = catchName(phrase)
+count = 1
+while(client is None and count < 3): #Three tries, then they get Bubbles.
+    phrase = input("I'm sorry, I misunderstood something. Could you tell me your name again please?\n")
+    client = catchName(phrase)
+    count+=1
+if(client is None):
+    print("I'm going to call you Bubbles.")
+    client = "Bubbles"
+    
+'''------------Start Convo------------''' #Consider alternate greetings, alternate phrases
+waiting = input("Greetings, " + client + ". How are you feeling today?\n")
 
+'''------------Respond to base state------------'''
